e1ea5d3482c5af4040c789778679c2e444f6d90e
Mason France <masonrf1@gmail.com>
2019-04-30 13:19:15 -0600

    First commit - Add data and basic structure

---
+++ db/composerData.js
@@ -0,0 +1,202 @@
+const composerData = [
+  {
+    name: 'Johann Sebastian Bach',
+    nationality: 'German',
+    lifeSpan: '1685-1750',
+    choralWorks: [
+      {
+        name: 'Matthaus-Passion',
+        arrangedFor: 'Sop solo, Alto solo, 2 Tenor solo, 2 Bass solo, 2 SATB Choir, 2 Orch'
+      },
+      {
+        name: 'St. John Passion',
+        arrangedFor: 'Sop solo, Alto solo, 2 Tenor solo, 2 Bass solo, SATB Choir, Orchestra'
+      },
+      {
+        name: 'Komm, Jesu, komm',
+        arrangedFor: 'SATB Choir, oboe, 2 English horn, bassoon, strings, basso continuo'
+      },
+      {
+        name: 'Singet dem Herrn ein neues Lied',
+        arrangedFor: '2 SATB Choir'
+      },
+      {
+        name: 'Magnificat D major',
+        arrangedFor: '2 Sop solo, Alto solo, Tenor solo, Bass solo, Mixed choir: SSATB, 2 Fl, 2 Ob(2 Ob-dam), 3Trp, timpani, 2 V, Va, Continuo(bassoon, Vc, Vo, Org)'
+      }
+    ]
+  },
+  {
+    name: 'Antonio Vivaldi',
+    nationality: 'Italian',
+    lifeSpan: '1678-1741',
+    choralWorks: [
+      {
+        name: 'Gloria',
+        arrangedFor: '2 Sop solo, Alto solo, SATB choir, Trumpet, Ob, 2 Violins, Viola, Continuo (Cello, double bass, Organ)'
+      },
+      {
+        name: 'Magnificat',
+        arrangedFor: 'SATB choir'
+      },
+      {
+        name: 'Sing to the Lord',
+        arrangedFor: 'SATB choir'
+      }
+    ]
+  },
+  {
+    name: 'George Frideric Handel',
+    nationality: 'German',
+    lifeSpan: '1685-1759',
+    choralWorks: [
+      {
+        name: 'Joy to the World',
+        arrangedFor: 'SATB choir'
+      },
+      {
+        name: 'Messiah',
+        arrangedFor: 'SATB Choir, oboes, trumpets, timpani, strings and basso continuo (harpsichord, violoncello, violone, bassoon)'
+      },
+      {
+        name: 'Praise the Lord',
+        arrangedFor: 'SAB choir'
+      }
+    ]
+  },
+  {
+    name: 'Henry Purcell',
+    nationality: 'English',
+    lifeSpan: '1659-1695',
+    choralWorks: [
+      {
+        name: 'Sing for Joy',
+        arrangedFor: 'SAB choir'
+      },
+      {
+        name: 'Anthem of Joy',
+        arrangedFor: 'SATB choir, trumpet'
+      },
+      {
+        name: 'If Music Be the Food of Love',
+        arrangedFor: 'SAB choir'
+      }
+    ]
+  },
+  {
+    name: 'Claudio Monteverdi',
+    nationality: 'Italian',
+    lifeSpan: '1567-1643',
+    choralWorks: [
+      {
+        name: 'Vespers',
+        arrangedFor: 'SSTTBB soloists, SATTB choir, orchestra (2/3 violins, 3 trumpets in D, 1 trumpet in B flat, 2/3 violas, 2 cellos, 1 double bass, 3 trombones, 1 bass trombone, 2 recorders, 2 flutes) '
+      },
+      {
+        name: "L'Orfeo",
+        arrangedFor: '7 Sop solo, 4 Tenor solo, 3 Bass solo, SSATTB choir, Orch'
+      },
+      {
+        name: 'Sestina',
+        arrangedFor: 'SATBB choir'
+      }
+    ]
+  },
+  {
+    name: 'Heinrich Schütz',
+    nationality: 'German',
+    lifeSpan: '1585-1672',
+    choralWorks: [
+      {
+        name: 'German Requiem',
+        arrangedFor: 'SATB choir, piano'
+      },
+      {
+        name: 'God Is Our Refuge',
+        arrangedFor: 'SATB Choir'
+      },
+      {
+        name: 'Sing a New Song',
+        arrangedFor: 'SATB choir, Flute, String bass'
+      }
+    ]
+  },
+  {
+    name: 'Domenico Scarlatti',
+    nationality: 'Italian',
+    lifeSpan: '1685-1757',
+    choralWorks: [
+      {
+        name: 'We Will Sing for Joy',
+        arrangedFor: 'Unison choir'
+      },
+      {
+        name: 'Capriccio Fugato Recorders',
+        arrangedFor: 'SAATB choir, 10 recorders'
+      },
+      {
+        name: 'Messa breve',
+        arrangedFor: 'SATB choir'
+      }
+    ]
+  },
+  {
+    name: 'Jean-Philippe Rameau',
+    nationality: 'French',
+    lifeSpan: '1683-1764',
+    choralWorks: [
+      {
+        name: 'Platee Chantons Bacchus',
+        arrangedFor: 'SATB'
+      },
+      {
+        name: 'Tambourine',
+        arrangedFor: 'SATB choir'
+      },
+      {
+        name: 'Laboravi Clamans',
+        arrangedFor: 'SSATB choir, organ'
+      }
+    ]
+  },
+  {
+    name: 'Arcangelo Corelli',
+    nationality: 'Italian',
+    lifeSpan: '1653-1713',
+    choralWorks: [
+      {
+        name: 'Pastorale',
+        arrangedFor: 'SAB choir, organ'
+      },
+      {
+        name: 'In Thee, O Lord, Have I Trusted',
+        arrangedFor: ' SATB choir'
+      },
+      {
+        name: 'O Magnum Mysterium',
+        arrangedFor: 'SATB voices'
+      }
+    ]
+  },
+  {
+    name: 'Georg Philipp Telemann',
+    nationality: 'German',
+    lifeSpan: '1681-1767',
+    choralWorks: [
+      {
+        name: 'St. John Passion',
+        arrangedFor: '2 Sop solo, Alto solo, 3 Tenor solo, 3 bass solo, SATB choir, piano'
+      },
+      {
+        name: 'O Lord, You Know Me',
+        arrangedFor: 'Unison choir'
+      },
+      {
+        name: 'Alleluia',
+        arrangedFor: 'SATB choir, 2 flutes'
+      }
+    ]
+  }
+]
+
+module.exports = composerData
